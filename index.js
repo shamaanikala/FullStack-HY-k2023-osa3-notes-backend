c3f1ec0c4aa07d599d87f7a1730430da089815fd
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-11 12:55:07 +0200

    Virheidenkäsittelijä middleware

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
@@ -53,7 +64,7 @@ let notes = [
     console.log('GET /api/notes received',Date())
   })
 
-  app.get('/api/notes/:id', (request, response) => {
+  app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
     .then(note => {
       if (note) {
@@ -63,21 +74,7 @@ let notes = [
         response.status(404).end()
       }
     })
-    .catch(error => {
-      //console.log(error)
-      //const syy = new mongoose.Error(error.message)
-      //console.log(syy)
-      //console.log(syy instanceof mongoose.Error.CastError )
-      //console.log(error instanceof mongoose.Error.CastError )
-      // omaa testiä
-      if (error instanceof mongoose.Error.CastError) {
-        console.log('CastError havaittu')
-        response.status(400).send({ error: 'malformatted id'})
-      }
-      else {
-      response.status(500).end()
-      }
-    })
+    .catch(error => next(error))
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -124,7 +121,7 @@ let notes = [
 
   // tämä vain tänne ja tuo ylös
   app.use(unknownEndpoint)
-  
+  app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
